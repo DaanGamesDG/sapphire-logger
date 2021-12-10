71069acab02a34f6ba50f8451622fccf90cf8d72
DaanGamesDG <daan@daangamesdg.xyz>
2021-12-10 21:59:27 +0100

    chore: remove un-wanted tests folder

--- tests/index.ts
+++
@@ -1,9 +0,0 @@
-import { LogLevel } from "@sapphire/framework";
-import { Logger } from "../src";
-const logger = new Logger({ level: LogLevel.Trace });
-logger.info("Hello");
-logger.debug("Hello");
-logger.trace("Hello");
-logger.error("Hello");
-logger.warn("Hello");
-logger.fatal("Hello");
